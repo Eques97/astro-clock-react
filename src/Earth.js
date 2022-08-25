17fb5a06076813251a370a0e63489ae1605a00bf
Eques97 <heinhtetthuta@gmail.com>
2022-08-25 09:52:50 +0630

    Added camera controller

--- src/Earth.js
+++ src/Earth.js
@@ -7,11 +7,9 @@ export default function Earth(props) {
     const EARTH_RADIUS = 6.371;
     const textureMap = useLoader(TextureLoader, earthImg);
     return (
-    <mesh
-        {...props}
-    >
-        <sphereGeometry args={[EARTH_RADIUS, 32, 32]} />
-        <meshStandardMaterial map={textureMap} />
+    <mesh {...props} >
+    <sphereGeometry args={[EARTH_RADIUS, 32, 32]} />
+    <meshStandardMaterial map={textureMap} />
     </mesh>
     )
 }
--- src/Scene.js
+++ src/Scene.js
@@ -1,20 +1,23 @@
 import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber';
+import CameraController from './CameraController';
 import Earth from './Earth'
 import Sun from './Sun';
 
+
 export default function Scene({timestamps}) {
-  console.log(timestamps);
   const SECONDS_IN_YEAR = 31558149.7635456;
   const SECONDS_IN_SIDEREAL_DAY = 86164.1;
   const dayGauge = timestamps % SECONDS_IN_SIDEREAL_DAY / SECONDS_IN_SIDEREAL_DAY;
   const yearGauge = timestamps % SECONDS_IN_YEAR / SECONDS_IN_YEAR;
-  console.log(dayGauge, yearGauge);
+  // console.log(dayGauge, yearGauge);
   return (
-    <div id="canvas-container" style={{ width: 1080, height: 720, backgroundColor: 'black' }} >
+    <div id="canvas-container" style={{ width: 240, height: 240, backgroundColor: 'black' }} >
     <Canvas camera={{position: [-15, 0, 0]}} >
-    <Earth rotation={[0, dayGauge * Math.PI, 0]} />
+    <CameraController />
+    <ambientLight intensity={0.1} />
     <Sun rotation={[0, yearGauge * Math.PI * 2, 0]} />
+    <Earth rotation={[0, dayGauge * Math.PI * 2, 0]} />
     </Canvas>
     </div>
   )
