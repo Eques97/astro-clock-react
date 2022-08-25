c76828274beb9ec8adf88d04dc8ddbc9eac85cfb
Eques97 <heinhtetthuta@gmail.com>
2022-08-25 11:01:24 +0630

    Added summer solstice offset

--- src/Scene.js
+++ src/Scene.js
@@ -6,19 +6,16 @@ import Sun from './Sun';
 
 
 export default function Scene({timestamps}) {
-  const SECONDS_IN_YEAR = 31558149.7635456;
-  const SECONDS_IN_SIDEREAL_DAY = 86164.1;
-  const dayGauge = timestamps % SECONDS_IN_SIDEREAL_DAY / SECONDS_IN_SIDEREAL_DAY;
-  const yearGauge = timestamps % SECONDS_IN_YEAR / SECONDS_IN_YEAR;
-  // console.log(dayGauge, yearGauge);
+  const SOLSTICE_OFFSET = 14774400;
+  timestamps -= SOLSTICE_OFFSET;
   return (
     <div id="canvas-container" style={{ width: 240, height: 240, backgroundColor: 'black' }} >
     <Canvas camera={{position: [-15, 0, 0]}} >
     <CameraController />
-    <ambientLight intensity={0.1} />
-    <Sun rotation={[0, yearGauge * Math.PI * 2, 0]} />
-    <Earth rotation={[0, dayGauge * Math.PI * 2, 0]} />
+    <ambientLight intensity={0.01} />
+    <Sun timestamps={timestamps} />
+    <Earth timestamps={timestamps} />
     </Canvas>
     </div>
   )
-}
+};
