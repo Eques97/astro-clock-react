5e34a30274f7b2bba24173d4238be725512de832
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2022-08-26 13:36:08 +0630

    add date input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Scene from './Scene';
+import "./App.css";
+import Scene from "./Scene";
 
 function App() {
-  const date = new Date("2022-06-21T06:00:00.000+00:00");
-  const timestamps = date.getTime() / 1000;
-  return (
-    <Scene timestamps={timestamps} />
-  );
+  return <Scene />;
 }
 
 export default App;
--- src/Scene.js
+++ src/Scene.js
@@ -1,21 +1,48 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber';
-import CameraController from './CameraController';
-import Earth from './Earth'
-import Sun from './Sun';
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import CameraController from "./CameraController";
+import Earth from "./Earth";
+import Sun from "./Sun";
+import { DatePicker } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
 
-
-export default function Scene({timestamps}) {
+export default function Scene({}) {
   const SOLSTICE_OFFSET = 14774400;
-  timestamps -= SOLSTICE_OFFSET;
+  const [date, setDate] = useState(new Date());
+
+  const handleChange = (e) => {
+    if (e) setDate(e);
+  };
+
+  const getTimeStamps = () => {
+    let timestamps = date.getTime() / 1000;
+    timestamps -= SOLSTICE_OFFSET;
+    return timestamps;
+  };
+
   return (
-    <div id="canvas-container" style={{ width: 240, height: 240, backgroundColor: 'black' }} >
-    <Canvas camera={{position: [-15, 0, 0]}} >
-    <CameraController />
-    <ambientLight intensity={0.01} />
-    <Sun timestamps={timestamps} />
-    <Earth timestamps={timestamps} />
-    </Canvas>
+    <div
+      id="canvas-container"
+      style={{ width: "100%", height: "100vh", backgroundColor: "black" }}
+    >
+      <DatePicker
+        format="yyyy-MM-dd HH:mm:ss"
+        calendarDefaultDate={new Date("2022-02-02 00:00:00")}
+        ranges={[
+          {
+            label: "Now",
+            value: new Date(),
+          },
+        ]}
+        style={{ width: 260 }}
+        onChange={(e) => handleChange(e)}
+      />
+      <Canvas camera={{ position: [-15, 0, 0] }}>
+        <CameraController />
+        <ambientLight intensity={0.01} />
+        <Sun timestamps={getTimeStamps()} />
+        <Earth timestamps={getTimeStamps()} />
+      </Canvas>
     </div>
-  )
-};
+  );
+}
