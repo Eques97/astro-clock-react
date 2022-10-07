c0c2437434fe9e86ba7fb8f641d271d93e909dfc
innoml <heinhtetthuta@gmail.com>
2022-10-07 17:06:13 +0630

    move scene to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
-import "./App.css";
-import Scene from "./Scene";
+import "./App.css"
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import CameraController from "./CameraController";
+import Earth from "./Earth";
+import Sun from "./Sun";
+import { DatePicker, SelectPicker } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
+import { unitData } from "./DataExport";
 
-function App() {
-  return <Scene />;
+export default function App({}) {
+  const SOLSTICE_OFFSET = 14774400;
+  const [date, setDate] = useState(new Date());
+  const [timeFactor, setTimeFactor] = useState(1);
+
+  const handleChange = (e) => {
+    if (e) setDate(e);
+  };
+
+  const getTimeStamps = () => {
+    let timestamps = date.getTime() / 1000;
+    timestamps -= SOLSTICE_OFFSET;
+    return timestamps;
+  };
+
+  const handleSelect = (e) => {
+    if (e) setTimeFactor(e);
+  };
+
+  return (
+    <div
+      id="canvas-container"
+      style={{ width: "100%", height: "100vh", backgroundColor: "black" }}
+    >
+      <div>
+        <DatePicker
+          format="yyyy-MM-dd HH:mm:ss"
+          calendarDefaultDate={new Date("2022-02-02 00:00:00")}
+          ranges={[
+            {
+              label: "Now",
+              value: new Date(),
+            },
+          ]}
+          style={{ width: 260 }}
+          onChange={(e) => handleChange(e)}
+        />
+        {/* <SelectPicker
+          data={unitData}
+          searchable={false}
+          style={{ width: 224 }}
+          onChange={(e) => handleSelect(e)}
+        /> */}
+      </div>
+      <Canvas camera={{ position: [15, 0, 0] }}>
+        {/* <CameraController /> */}
+        <ambientLight intensity={0.01} />
+        <Sun timestamps={getTimeStamps()} timeFactor={timeFactor} />
+        <Earth timestamps={getTimeStamps()} timeFactor={timeFactor} />
+      </Canvas>
+    </div>
+  );
 }
-
-export default App;
