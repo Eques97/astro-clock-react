db55c694afd0a87312182eee3530bbecfc921aaa
innoml <heinhtetthuta@gmail.com>
2022-10-30 19:02:39 +0630

    add local useeffect timer

--- src/Clock.js
+++ src/Clock.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-export default function Clock({timestamps}) {
-    let date = new Date(timestamps);
+export default function Clock({baseTimestamps, timefactor}) {
+    const [timestamps, setTimestamps] = useState(0);
+    useEffect(() => {
+      setTimestamps(baseTimestamps);
+      let interval = setInterval(() => {
+        setTimestamps(prevTimestamps => prevTimestamps + 1000 * timefactor);
+      }, 1000);
+      return () => clearInterval(interval);
+    },[baseTimestamps, timefactor]);
+    const date = new Date(timestamps);
     return (
     <div style={{position:"fixed", textAlign:"center", left:0, color:"white", fontSize:"100%", zIndex: 1}}>{date.toString()}</div>
     )
